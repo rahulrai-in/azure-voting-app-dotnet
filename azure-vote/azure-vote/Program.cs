512c7280aef4b1144d1c016a0fb81a36ab73a819
Rahul Rai <rahulrai@live.com>
2022-06-13 16:47:51 +1000

    Enable OTLP exporter through configuration

--- azure-vote/azure-vote/Program.cs
+++ azure-vote/azure-vote/Program.cs
@@ -13,79 +13,90 @@ builder.Services.AddSingleton<IConnectionMultiplexer>(redisConnection);
 builder.Services.Configure<VoteAppSettings>(builder.Configuration.GetSection(nameof(VoteAppSettings)));
 
 // Configure OpenTelemetry tracing
-builder.Services.AddOpenTelemetryTracing(tracerProviderBuilder => tracerProviderBuilder
-    // Customize the traces gathered by the HTTP request handler
-    .AddAspNetCoreInstrumentation(options =>
+builder.Services.AddOpenTelemetryTracing(tracerProviderBuilder =>
     {
-        options.RecordException = true;
-        // Add metadata for the request such as the HTTP method and response length
-        options.Enrich = (activity, eventName, rawObject) =>
-        {
-            switch (eventName)
+        tracerProviderBuilder
+            // Customize the traces gathered by the HTTP request handler
+            .AddAspNetCoreInstrumentation(options =>
             {
-                case "OnStartActivity":
+                options.RecordException = true;
+                // Add metadata for the request such as the HTTP method and response length
+                options.Enrich = (activity, eventName, rawObject) =>
                 {
-                    if (rawObject is not HttpRequest httpRequest)
+                    switch (eventName)
                     {
-                        return;
-                    }
+                        case "OnStartActivity":
+                        {
+                            if (rawObject is not HttpRequest httpRequest)
+                            {
+                                return;
+                            }
 
-                    activity.SetTag("requestProtocol", httpRequest.Protocol);
-                    activity.SetTag("requestMethod", httpRequest.Method);
-                    break;
-                }
-                case "OnStopActivity":
-                {
-                    if (rawObject is HttpResponse httpResponse)
-                    {
-                        activity.SetTag("responseLength", httpResponse.ContentLength);
-                    }
+                            activity.SetTag("requestProtocol", httpRequest.Protocol);
+                            activity.SetTag("requestMethod", httpRequest.Method);
+                            break;
+                        }
+                        case "OnStopActivity":
+                        {
+                            if (rawObject is HttpResponse httpResponse)
+                            {
+                                activity.SetTag("responseLength", httpResponse.ContentLength);
+                            }
 
-                    break;
-                }
-            }
-        };
-    })
-    // Instrument Redis client
-    .AddRedisInstrumentation(redisConnection, opt =>
-    {
-        opt.FlushInterval = TimeSpan.FromSeconds(1);
-        opt.SetVerboseDatabaseStatements = true;
-    })
-    .AddSource("my-corp.azure-vote.vote-app")
-    // Create resources (key-value pairs) that describe your service such as service name and version
-    .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("vote-app")
-        .AddAttributes(new[] { new KeyValuePair<string, object>("service.version", "1.0.0.0") }))
-    // Ensures that all activities are recorded and sent to exporter
-    .SetSampler(new AlwaysOnSampler())
-    // Displays traces on the console. Useful for debugging.
-    .AddConsoleExporter()
-    // Exports spans to an OTLP endpoint. Use this for exporting traces to collector or a backend that support OTLP over HTTP
-    //.AddOtlpExporter(otlpOptions =>
-    //{
-    //    otlpOptions.Endpoint = new("http://localhost:4318/v1/traces");
-    //    otlpOptions.Protocol = OtlpExportProtocol.HttpProtobuf;
-    //})
+                            break;
+                        }
+                    }
+                };
+            })
+            // Instrument Redis client
+            .AddRedisInstrumentation(redisConnection, opt =>
+            {
+                opt.FlushInterval = TimeSpan.FromSeconds(1);
+                opt.SetVerboseDatabaseStatements = true;
+            })
+            .AddSource("my-corp.azure-vote.vote-app")
+            // Create resources (key-value pairs) that describe your service such as service name and version
+            .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("vote-app")
+                .AddAttributes(new[] { new KeyValuePair<string, object>("service.version", "1.0.0.0") }))
+            // Ensures that all activities are recorded and sent to exporter
+            .SetSampler(new AlwaysOnSampler())
+            // Displays traces on the console. Useful for debugging.
+            .AddConsoleExporter();
+        if (builder.Configuration.GetValue<bool>("EnableOtlpExporter"))
+        {
+            // Exports spans to an OTLP endpoint. Use this for exporting traces to collector or a backend that support OTLP over HTTP
+            tracerProviderBuilder.AddOtlpExporter(otlpOptions =>
+            {
+                otlpOptions.Endpoint = new("http://localhost:4318/v1/traces");
+                otlpOptions.Protocol = OtlpExportProtocol.HttpProtobuf;
+            });
+        }
+    }
 );
 
 builder.Services.AddSingleton(new ActivitySource("my-corp.azure-vote.vote-app"));
 
 // Configure OpenTelemetry metrics
 builder.Services.AddOpenTelemetryMetrics(meterProviderBuilder =>
-    meterProviderBuilder.AddAspNetCoreInstrumentation()
-        .AddMeter("my-corp.azure-vote.vote-app")
-        // Create resources (key-value pairs) that describe your service such as service name and version
-        .SetResourceBuilder(
-            ResourceBuilder.CreateDefault().AddService("vote-app")
-                .AddAttributes(new[] { new KeyValuePair<string, object>("service.version", "1.0.0.0") }))
-        // Displays metrics on the console. Useful for debugging.
-        .AddConsoleExporter()
-        // Exports metrics to an OTLP endpoint. Use this for exporting metrics to collector or a backend that support OTLP over HTTP
-        //.AddOtlpExporter(otlpOptions =>
-        //{
-        //    otlpOptions.Endpoint = new("http://localhost:4318/v1/metrics");
-        //    otlpOptions.Protocol = OtlpExportProtocol.HttpProtobuf;
-        //})
+    {
+        meterProviderBuilder.AddAspNetCoreInstrumentation()
+            .AddMeter("my-corp.azure-vote.vote-app")
+            // Create resources (key-value pairs) that describe your service such as service name and version
+            .SetResourceBuilder(
+                ResourceBuilder.CreateDefault().AddService("vote-app")
+                    .AddAttributes(new[] { new KeyValuePair<string, object>("service.version", "1.0.0.0") }))
+            // Displays metrics on the console. Useful for debugging.
+            .AddConsoleExporter();
+        if (builder.Configuration.GetValue<bool>("EnableOtlpExporter"))
+        {
+            // Exports metrics to an OTLP endpoint. Use this for exporting metrics to collector or a backend that support OTLP over HTTP
+            meterProviderBuilder.AddOtlpExporter(otlpOptions =>
+            {
+                otlpOptions.Endpoint = new("http://localhost:4318/v1/metrics");
+                otlpOptions.Protocol = OtlpExportProtocol.HttpProtobuf;
+            });
+        }
+    }
 );
 
 builder.Services.AddSingleton(new Meter("my-corp.azure-vote.vote-app"));
